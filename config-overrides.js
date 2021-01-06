ed3eaa4aef62e80d5310a64809a26f8bad9ff684
Steven-Rogers27 <qitiandasheng5259@126.com>
2021-01-06 22:52:08 +0800

    解决IE11兼容性

--- config-overrides.js
+++ config-overrides.js
@@ -12,58 +12,50 @@ module.exports = {
       ...config.resolve.alias,
       '@': path.resolve(__dirname, 'src'),
     };
+    // 为了兼容IE11，据说是react和react-dom的所依赖的Symbol polyfill版本不一样，需要强制依次引入core-js（因为@babel/polyfill即将作废，其内部
+    // 还是引用的core-js和regenerator-runtime）、react、react-dom，以保证react和react-dom使用同样的Symbol polyfill
+    // issue: https://stackoverflow.com/questions/40897966/objects-are-not-valid-as-a-react-child-in-internet-explorer-11-for-react-15-4-1
     config.entry = [
-      '@babel/polyfill',
       'react',
       'react-dom',
       config.entry,
     ];
-
+    // 处理src目录下的js文件的babel-loader配置
     const babelLoader = getBabelLoader(config);
     babelLoader.options.presets.push(
       [
         '@babel/env',
         {
           useBuiltIns: 'usage',
-          corejs: '2',
+          corejs: '3',
+          targets: {
+            ie: 9,
+          },
           modules: 'umd',
         }
-      ]
+      ],
+      [
+        '@babel/preset-react',
+        {
+          development: process.env.NODE_ENV === 'development',
+        }
+      ],
+      [
+        '@babel/preset-typescript',
+        {
+          isTSX: true,
+          allExtensions: true,
+        }
+      ],
     );
-    fs.writeFile(path.resolve(__dirname, 'babel-loader-config.json'), JSON.stringify(babelLoader), err => {
-      console.error(err);
-    });
-    console.log(babelLoader);
-
+    // 处理src目录以外的js文件的babel-loader配置
     const babelLoaderOuter = getBabelLoader(config, true);
     babelLoaderOuter.options.plugins = babelLoaderOuter.options.plugins || [];
     babelLoaderOuter.options.plugins.push(
-      // [
-      //   '@babel/plugin-transform-arrow-functions',
-      // ],
       [
         '@babel/plugin-transform-runtime'
       ],
     );
-    // babelLoaderOuter.options.presets.push(
-    //   [
-    //     '@babel/env',
-    //     {
-    //       useBuiltIns: 'usage',
-    //       corejs: '2',
-    //       modules: 'umd',
-    //     }
-    //   ]
-    // );
-    fs.writeFile(path.resolve(__dirname, 'babel-loader-outer.json'), JSON.stringify(babelLoaderOuter), err => {
-      console.error(err);
-    })
-    console.log(babelLoaderOuter);
-
-
-    fs.writeFile(path.resolve(__dirname, 'webpack.config.json'), JSON.stringify(config), err => {
-      console.error(err);
-    })
 
     return config;
   },
