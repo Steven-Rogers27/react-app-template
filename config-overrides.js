8f60865b6cf6bf6a7eda9231e211e3c7fb524d03
孙伟(10034471) <sunw-d@glodon.com>
2021-01-06 18:09:18 +0800

    增加babel配置

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,8 @@
 const path = require('path');
+const fs = require('fs');
+const {
+  getBabelLoader,
+} = require('customize-cra');
 
 module.exports = {
   webpack: function(config, env) {
@@ -8,6 +12,59 @@ module.exports = {
       ...config.resolve.alias,
       '@': path.resolve(__dirname, 'src'),
     };
+    config.entry = [
+      '@babel/polyfill',
+      'react',
+      'react-dom',
+      config.entry,
+    ];
+
+    const babelLoader = getBabelLoader(config);
+    babelLoader.options.presets.push(
+      [
+        '@babel/env',
+        {
+          useBuiltIns: 'usage',
+          corejs: '2',
+          modules: 'umd',
+        }
+      ]
+    );
+    fs.writeFile(path.resolve(__dirname, 'babel-loader-config.json'), JSON.stringify(babelLoader), err => {
+      console.error(err);
+    });
+    console.log(babelLoader);
+
+    const babelLoaderOuter = getBabelLoader(config, true);
+    babelLoaderOuter.options.plugins = babelLoaderOuter.options.plugins || [];
+    babelLoaderOuter.options.plugins.push(
+      // [
+      //   '@babel/plugin-transform-arrow-functions',
+      // ],
+      [
+        '@babel/plugin-transform-runtime'
+      ],
+    );
+    // babelLoaderOuter.options.presets.push(
+    //   [
+    //     '@babel/env',
+    //     {
+    //       useBuiltIns: 'usage',
+    //       corejs: '2',
+    //       modules: 'umd',
+    //     }
+    //   ]
+    // );
+    fs.writeFile(path.resolve(__dirname, 'babel-loader-outer.json'), JSON.stringify(babelLoaderOuter), err => {
+      console.error(err);
+    })
+    console.log(babelLoaderOuter);
+
+
+    fs.writeFile(path.resolve(__dirname, 'webpack.config.json'), JSON.stringify(config), err => {
+      console.error(err);
+    })
+
     return config;
   },
 
